22888991e0a1c66877d3aa1a7a01458f997fceed
HeroProtagonist <jstepper91@gmail.com>
2016-10-04 01:13:32 -0400

    Enable sample action

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import handleClick from '../redux/actions/clickActions';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        Channel Surfer
+      <div onClick={() => this.props.handleClick(!this.props.click)}>
+        Channel Surfer!
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  click: state.click,
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    handleClick: bindActionCreators(handleClick, dispatch),
+  };
+}
+
+App.propTypes = {
+  click: React.PropTypes.bool,
+  handleClick: React.PropTypes.func,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App.jsx';
-// import configureStore from './redux/store/configureStore';
+import configureStore from './redux/store/configureStore';
 
-// const store = configureStore();
+const store = configureStore();
 
 render(
-  // <Provider store={store}>
-    <App />,
-  // </Provider>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
