b55d40b65586069a3d87f30710ade82c0cb42bbe
HeroProtagonist <jstepper91@gmail.com>
2016-10-04 00:15:29 -0400

    Initial commit

---
+++ webpack.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+
+// Load environment variables
+const env = require('./env/client');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './src/index.jsx',
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(env.NODE_ENV),
+      },
+    }),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['react', 'es2015', 'stage-0', 'react-hmre'],
+        },
+      },
+    ],
+  },
+};
